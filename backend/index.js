a94aecc8d2ea50d08fb70571e7a911370816bc1c
anirudh-2704 <s.anirudh.official@gmail.com>
2025-07-20 13:07:11 +0530

    ready for production

--- backend/index.js
+++ backend/index.js
@@ -1,4 +1,4 @@
-import express from "express";
+import express, {urlencoded} from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
@@ -7,14 +7,21 @@ import userRoute from "./routes/user.route.js";
 import companyRoute from "./routes/company.route.js";
 import jobRoute from "./routes/job.route.js";
 import applicationRoute from "./routes/application.route.js"; 
+import path from "path";
+import bodyParser from "body-parser";
 
 dotenv.config({});
+connectDB();
+const PORT=process.env.PORT || 8080;
 
 const app=express();
 
+const __dirname = path.resolve();
+
 //middleware
 app.use(express.json());
-app.use(express.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(urlencoded({extended:true}));
 app.use(cookieParser());
 const corsOptions={
   origin:"http://localhost:5173",
@@ -22,16 +29,18 @@ const corsOptions={
 }
 app.use(cors(corsOptions));
 
-const PORT=process.env.PORT || 3000;
-
-
 // api's 
 app.use("/api/v1/user",userRoute);
 app.use("/api/v1/company",companyRoute);
 app.use("/api/v1/job",jobRoute);
 app.use("/api/v1/application", applicationRoute); 
 
+app.use(express.static(path.join(__dirname, "/frontend/dist")));
+app.get('',(req,res)=>{
+  res.sendFile(path.resolve(__dirname,"frontend","dist", "index.html"));
+})
+
 app.listen(PORT,()=>{
-  connectDB();
+  // connectDB();
   console.log(`Server is running on http://localhost:${PORT}`);
 })
